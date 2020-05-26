0ccfaca5de92dc381dfd30b552e67bcf8168a135
Antonio Lorusso <mitictonylor@gmail.com>
2020-05-26 16:55:51 +0100

    add getGere() test pass

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    @Test
+    public void getBookGenre(){
+        Book book = new Book("I promessi sposi", "A. Manzoni", "classic");
+        assertEquals("classic", book.getGenre());
+    }
 }
